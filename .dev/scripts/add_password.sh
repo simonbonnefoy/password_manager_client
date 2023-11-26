3add9dc1c62c6718b5351c6dcbcbb9a3eed6f538
simon <bonnefoy.simon@protonmail.com>
2023-11-26 16:12:56 +0100

    fixed key in test scripts

--- .dev/scripts/add_password.sh
+++ .dev/scripts/add_password.sh
@@ -3,4 +3,4 @@ ROOT_FOLDER=$(git rev-parse --show-toplevel)
 
 RANDOM_TITLE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 
-python main.py --action add_password --email toto@toto.com --title $RANDOM_TITLE --username bob --password 1234 --notes notes --key mysecrectkey
+python main.py --action add_password --email toto@toto.com --title $RANDOM_TITLE --username bob --password 1234 --notes notes --key my_secret_key
--- .dev/scripts/get_passwords_by_id.sh
+++ .dev/scripts/get_passwords_by_id.sh
@@ -1,4 +1,4 @@
 # Get repo root folder
 ROOT_FOLDER=$(git rev-parse --show-toplevel)
 
-python $ROOT_FOLDER/main.py --action get_password --id 1 --key my_secret_key
+python $ROOT_FOLDER/main.py --action get_password --id 4 --key my_secret_key
